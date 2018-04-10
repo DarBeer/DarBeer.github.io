b8076bdae4eb9566956d57ea5a2549db744af189
Sancho <crazy.sanchos5@gmail.com>
2018-04-11 01:18:55 +0300

    spaces

--- src/data/images.ts
+++ src/data/images.ts
@@ -2,11 +2,10 @@ export class Image {
     id: number;
     url: string;
     data: string;
-
 }
 
 export const IMAGES: Image[] = [
-    { id: 0 ,url: '../../assets/img/1.jpg', data: '10.04.2018'},
+    { id: 0 ,url: '../../assets/img/1.jpg', data: '10.04.2018' },
     { id: 1, url: '../../assets/img/2.jpg', data: '10.04.2018' },
     { id: 2, url: '../../assets/img/3.jpg', data: '10.04.2018' },
     { id: 3, url: '../../assets/img/4.jpg', data: '10.04.2018' },
@@ -17,5 +16,5 @@ export const IMAGES: Image[] = [
     { id: 8, url: '../../assets/img/3.jpg', data: '10.04.2018' },
     { id: 9, url: '../../assets/img/4.jpg', data: '10.04.2018' },
     { id: 10, url: '../../assets/img/5.jpg', data: '10.04.2018' },
-    { id: 11, url: '../../assets/img/6.jpg', data: '10.04.2018'  }
+    { id: 11, url: '../../assets/img/6.jpg', data: '10.04.2018' }
 ];
