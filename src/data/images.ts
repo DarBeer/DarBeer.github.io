3740072ef579fdefae0404333feccaa955830b0d
Sancho <crazy.sanchos5@gmail.com>
2018-04-11 02:37:31 +0300

    style

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,21 +1,20 @@
 export class Image {
     id: number;
     urlImage: string;
-    data: string;
-
+    date: string;
 }
 
 export const IMAGES: Image[] = [
-    { id: 0, urlImage: '../../assets/img/1.jpg', data: '10.04.2018'},
-    { id: 1, urlImage: '../../assets/img/2.jpg', data: '10.04.2018' },
-    { id: 2, urlImage: '../../assets/img/3.jpg', data: '10.04.2018' },
-    { id: 3, urlImage: '../../assets/img/4.jpg', data: '10.04.2018' },
-    { id: 4, urlImage: '../../assets/img/5.jpg', data: '10.04.2018' },
-    { id: 5, urlImage: '../../assets/img/6.jpg', data: '10.04.2018' },
-    { id: 6, urlImage: '../../assets/img/1.jpg', data: '10.04.2018' },
-    { id: 7, urlImage: '../../assets/img/2.jpg', data: '10.04.2018' },
-    { id: 8, urlImage: '../../assets/img/3.jpg', data: '10.04.2018' },
-    { id: 9, urlImage: '../../assets/img/4.jpg', data: '10.04.2018' },
-    { id: 10, urlImage: '../../assets/img/5.jpg', data: '10.04.2018' },
-    { id: 11, urlImage: '../../assets/img/6.jpg', data: '10.04.2018' }
+    { id: 0, urlImage: '../../assets/img/1.jpg', date: '30.04.2018' },
+    { id: 1, urlImage: '../../assets/img/2.jpg', date: '31.03.2018' },
+    { id: 2, urlImage: '../../assets/img/3.jpg', date: '1.04.2018' },
+    { id: 3, urlImage: '../../assets/img/4.jpg', date: '2.04.2018' },
+    { id: 4, urlImage: '../../assets/img/5.jpg', date: '3.04.2018' },
+    { id: 5, urlImage: '../../assets/img/6.jpg', date: '4.04.2018' },
+    { id: 6, urlImage: '../../assets/img/1.jpg', date: '5.04.2018' },
+    { id: 7, urlImage: '../../assets/img/2.jpg', date: '6.04.2018' },
+    { id: 8, urlImage: '../../assets/img/3.jpg', date: '7.04.2018' },
+    { id: 9, urlImage: '../../assets/img/4.jpg', date: '8.04.2018' },
+    { id: 10, urlImage: '../../assets/img/5.jpg', date: '9.04.2018' },
+    { id: 11, urlImage: '../../assets/img/6.jpg', date: '10.04.2018' }
 ];
