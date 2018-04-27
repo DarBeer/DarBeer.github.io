b415a19d6c1b43458db676aa2ca25a5b7a03b8f9
Sancho <crazy.sanchos5@gmail.com>
2018-04-28 02:38:11 +0300

    imported service to main page

--- src/app/web/image-gallery/image-gallery.component.ts
+++ src/app/web/image-gallery/image-gallery.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Image, IMAGES } from '../../../data/images';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+//import { IMAGES } from '../../../data/images';
+import { DataService } from "../../shared/services/data.service";
+import { Image } from "../../dashboard/image-data/image";
+
 
 @Component({
     selector: 'app-image-gallery',
@@ -13,14 +15,22 @@ export class ImageGalleryComponent implements OnInit {
     heading = 'Галерея Экспонатов';
     subheading = 'Фотографии и их поиск';
 
-    images = IMAGES;
+    private errorMessage: string;
+    images: Image[];
+    image: Image;
+    //images = IMAGES;
     selectedImage: Image;
 
 
-    constructor(private modalService: NgbModal) {
+    constructor(private modalService: NgbModal, private service: DataService) {
     }
 
     ngOnInit() {
+        this.service.getImages()
+            .subscribe(
+                images => this.images = images,
+                error => this.errorMessage = error
+            )
     }
 
     openImage(content) {
