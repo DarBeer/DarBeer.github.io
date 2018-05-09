759770b73979fb1a940870c67930a71b42534940
Sancho <crazy.sanchos5@gmail.com>
2018-05-09 16:13:29 +0300

    image grid, style, search added/fix

--- src/app/web/image-gallery/image-gallery.component.ts
+++ src/app/web/image-gallery/image-gallery.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-//import { IMAGES } from '../../../data/images';
 import { DataService } from "../../shared/services/data.service";
 import { Image } from "../../dashboard/image-data/image";
 
@@ -18,9 +17,7 @@ export class ImageGalleryComponent implements OnInit {
     private errorMessage: string;
     images: Image[];
     image: Image;
-    //images = IMAGES;
-    selectedImage: Image;
-
+    imgString: string;
 
     constructor(private modalService: NgbModal, private service: DataService) {
     }
@@ -33,12 +30,31 @@ export class ImageGalleryComponent implements OnInit {
             )
     }
 
+    searchImage(value){
+        if(!value) this.service.getImages()
+            .subscribe(
+                images => this.images = images,
+                error => this.errorMessage = error
+            );
+        this.service.getImages()
+            .subscribe(
+                images => {
+                    this.images = Object.assign([], images).filter(
+                    image =>
+                        image.heading.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
+                        image.description.toLowerCase().indexOf(value.toLowerCase()) > -1
+                    )
+                },
+                error => this.errorMessage = error
+            )
+    }
+
     openImage(content) {
         this.modalService.open(content, { centered: true, size: 'lg' });
     }
 
     onSelect(image: Image): void {
-        this.selectedImage = image;
+        this.image = image;
     }
 
 }
--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
+import { OrderModule } from "ngx-order-pipe";
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { WebComponent } from './web.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +12,7 @@ import { HistoryComponent } from './history/history.component';
 import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
 import { VideoGalleryComponent } from './video-gallery/video-gallery.component';
 import { PageComponent } from './articles/page/page.component';
+
 
 @NgModule({
     declarations: [
@@ -22,7 +26,10 @@ import { PageComponent } from './articles/page/page.component';
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        OrderModule,
+        FormsModule,
+        NgxPaginationModule
     ],
     exports: [
         RouterModule
